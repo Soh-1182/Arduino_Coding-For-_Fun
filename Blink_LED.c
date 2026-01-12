c82aa11a6910cd46af6dced6d3b92f6a8050d934
Soh-1182 <sohankrishna.golkonda@gmail.com>
2026-01-11 17:18:05 -0800

    Creation of Blink project with arduino
    Creation of Blink project with Arduino

---
+++ Blink_LED.c
@@ -0,0 +1,23 @@
+/* This project is compatable with all microcontrollers in the Arduino series.
+  Tools needed are
+1. LED (Most Arduino boards have builtin led) If you don't want an separate LED, check the board, if it has an LED with an (L) next to it, then you don't need a separate LED.
+2. Resistors(220ohms, or 330 ohms). IF you are using builtin led, Skip this
+3. A microcontroller (Arduino).
+4. Breadboard
+5. 2 male to male juming wiries 
+
+*\
+
+int led = 13; 
+void setup() {
+
+  pinMode(led, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
